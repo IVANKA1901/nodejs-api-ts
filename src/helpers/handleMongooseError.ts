3959a6db98823d49b3efd6e1ed9be388d1133212
IVANKA1901 <125952478+IVANKA1901@users.noreply.github.com>
2023-07-22 19:27:06 +0200

    Update handleMongooseError.ts

--- src/helpers/handleMongooseError.ts
+++ src/helpers/handleMongooseError.ts
@@ -1,8 +1,19 @@
-const handleMongooseError = (error, data, next) => {
+import { Document } from "mongoose";
+
+interface IRequestError extends Error {
+  status?: number;
+  code?: number;
+}
+
+const handleMongooseError = (
+  error: IRequestError,
+  data: Document,
+  next: () => void
+) => {
   const { name, code } = error;
-  const status = name === "MongoServerError" && code === "11000" ? 409 : 400;
+  const status = name === "MongoServerError" && code === 11000 ? 409 : 400;
   error.status = status;
   next();
 };
 
-module.exports = handleMongooseError;
+export default handleMongooseError;
